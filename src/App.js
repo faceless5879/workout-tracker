ba2563f64856641157d376d58cabea75782f7866
Dat.PM <faceless5879@gmai.com>
2022-10-22 00:05:09 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BasicModal from "./components/BasicModal";
+
+function App() {
+  return (
+    <div className="App">
+      <BasicModal />
+    </div>
+  );
+}
+
+export default App;
