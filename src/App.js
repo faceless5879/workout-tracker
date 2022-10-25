176bbfec8acb8c83dea3eee44edae824e774e54d
Eric <eytam619@gmail.com>
2022-10-24 22:35:12 -0400

    feat: implement dayview component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import BasicModal from "./components/BasicModal";
+import DayView from "../src/components/dayView/DayView";
 
 function App() {
   return (
     <div className="App">
       <BasicModal />
+      <DayView />
     </div>
   );
 }
---
+++ src/components/dayView/dummyData.js
@@ -0,0 +1,16 @@
+const dummy = [
+    {
+        exercise: "pushups",
+        weight: 150,
+        sets: 4,
+        reps: 8
+    },
+    {
+        exercise: "squats",
+        weight: "n/a",
+        sets: 3,
+        reps: 25
+    }
+]
+
+export default dummy;
