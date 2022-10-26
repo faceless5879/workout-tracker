4160ce1d17b96ebc9f278b9cedfa0f5942079e83
Eric <eytam619@gmail.com>
2022-10-25 22:14:35 -0400

    feat: implement userProfile

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import BasicModal from "./components/BasicModal";
 import DayView from "../src/components/dayView/DayView";
+import UserProfile from "../src/components/userProfile/UserProfile";
 
 function App() {
   return (
     <div className="App">
       <BasicModal />
+      <UserProfile />
       <DayView />
     </div>
   );
