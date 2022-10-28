1ba6fd6555b4c7c7015fcf69818d74012749c14e
Raeki <mkeithley@gmail.com>
2022-10-28 17:57:50 +0900

    feat: create state to track which view to show

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import "./App.css";
-import DayView from "../src/components/dayView/DayView";
+import './App.css';
 import WeekView from './components/WeekView';
-import UserProfile from "../src/components/userProfile/UserProfile";
-import NavBar from "../src/components/navbar/NavBar";
-import { useState } from "react";
-
+import LoginSignup from './components/LoginSignup';
+import NavBar from '../src/components/navbar/NavBar';
+import { useState, useEffect } from 'react';
 
 function App() {
   const [displayProfile, setDisplayProfile] = useState(false);
+  const [view, setView] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem('userid')) {
+      setView(<WeekView setView={setView}></WeekView>);
+    } else {
+      <LoginSignup setView={setView}></LoginSignup>;
+    }
+  }, []);
+
   return (
-    <div className="App">
+    <div className='App'>
       {/* <BasicModal /> */}
-      <NavBar displayProfile={displayProfile} />
-      {displayProfile ? <UserProfile /> : null}
+      <NavBar
+        setView={setView}
+        setDisplayProfile={setDisplayProfile}
+        displayProfile={displayProfile}
+      />
+      <>{view}</>
     </div>
   );
 }
