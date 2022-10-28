71f744026e6066dfc6f168ce74da72c599620d89
Raeki <mkeithley@gmail.com>
2022-10-28 18:01:08 +0900

    feat: view profile and logout

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,16 +4,22 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import UserProfile from "../userProfile/UserProfile";
+import LoginSignup from "../LoginSignup";
 
-export default function ButtonAppBar(props) {
-  const { displayProfile } = props;
-  console.log(displayProfile);
+export default function NavBar({ setView, displayProfile, setDisplayProfile }) {
+  function logout() {
+    localStorage.clear();
+    setView(<LoginSignup setView={setView}></LoginSignup>)
+  }
   return (
     <Box style={{ display: "flex", justifyContent: "center" }}>
       <AppBar position="static">
         <Toolbar>
           <Typography variant="h6">Workout tracker</Typography>
-          <Button color="inherit" style={{ marginLeft: "auto" }}>
+          <Button color="inherit" style={{ marginLeft: "auto" }} onClick={() => {
+            displayProfile ? logout() : setView(<UserProfile setDisplayProfile={setDisplayProfile} setView={setView}></UserProfile>)
+          }}>
             {displayProfile ? "Logout" : "Profile"}
           </Button>
         </Toolbar>
--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,8 +1,10 @@
-import  React, { useEffect } from 'react';
+import React, { useEffect } from 'react';
 import "./userProfile.css";
 import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { styled } from '@mui/material/styles';
+import { Button } from '@mui/material';
+import WeekView from '../WeekView';
 
 const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(1),
@@ -10,25 +12,34 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 let calcBMI = (weight, height) => {
-  const heightInMeters = (height * height)/10000
-  return weight/heightInMeters;
+  const heightInMeters = (height * height) / 10000
+  return weight / heightInMeters;
 }
 
-export default function UserProfile() {
+export default function UserProfile({ setDisplayProfile, setView }) {
+
+  useEffect(() => {
+    setDisplayProfile(true);
+  })
+
   return (
     <div>
       <button className="home-btn">Home</button>
-    <div className="card">
-      <div>
-        <img className="image" alt="Workout Avatar" src={require("./image/f9b1e644099201a068b395489e96013a.jpeg")} />
+      <div className="card">
+        <div>
+          <img className="image" alt="Workout Avatar" src={require("./image/f9b1e644099201a068b395489e96013a.jpeg")} />
+        </div>
+        <h2>Rocky</h2>
+        <Stack direction="column" spacing={2}>
+          <Item>Height(cm): 182</Item>
+          <Item>Weight(kg): 90</Item>
+          <Item>BMI: {calcBMI(90, 182)}</Item>
+        </Stack>
       </div>
-      <h2>Rocky</h2>
-      <Stack direction="column" spacing={2}>
-        <Item>Height(cm): 182</Item>
-        <Item>Weight(kg): 90</Item>
-        <Item>BMI: {calcBMI(90, 182)}</Item>
-      </Stack>
-      </div>
+      <Button onClick={() => {
+        setDisplayProfile(false);
+        setView(<WeekView setView={setView}></WeekView>)
+      }}>Week View</Button>
     </div>
   )
 }
